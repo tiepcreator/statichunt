6ca4ce85d6d66837c4db11ef8d785a27e2a9b26f
Sojon <sojonatmail@gmail.com>
2021-12-05 17:08:33 +0600

    fixed responsive issues

--- themes/statichunt/assets/js/scripts.js
+++ themes/statichunt/assets/js/scripts.js
@@ -49,3 +49,21 @@ window.onload = function() {
     }
   }
 };
+
+// sidebar toggler
+var sidebarToggler = document.querySelector('.sidebar-toggler');
+var sidebarOverlay = document.querySelector('.sidebar-overlay');
+var sidebarTogglerIcon = document.querySelector('.sidebar-toggle-icon');
+var sidebar = document.querySelector('.sidebar');
+var body = document.querySelector('body');
+sidebarToggler.addEventListener('click', function() {
+  sidebarOverlay.classList.toggle('show');
+  sidebar.classList.toggle('show');
+  body.classList.toggle('overflow-hidden');
+});
+sidebarOverlay.addEventListener('click', function() {
+  this.classList.remove('show');
+  sidebarTogglerIcon.classList.remove('active');
+  sidebar.classList.remove('show');
+  body.classList.remove('overflow-hidden');
+});
